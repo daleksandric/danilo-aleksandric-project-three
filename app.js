f1981ab005ea5cca0c1c98c4f59b64ed726fc05c
Danilo Aleksandric <aleksandric.danilo@gmail.com>
2020-09-14 01:01:39 -0400

    working towrds making website responsive

--- app.js
+++ app.js
@@ -8,14 +8,36 @@ diceApp.showDice = (chosenDice) => {
     $('#diceResult').html(`<div>${chosenDice}</div>`);
 }
 
+diceApp.showRoll = (chosenDice, diceId) => {
+    const diceRolled = $('<p>').html(`Rolling 1d${diceId}`);
+    const numberResult = $('<p>').html(`${chosenDice}`);
+
+    $('#rollHistory').append(diceRolled, numberResult);
+}
+
+diceApp.showMainScreen = (chosenDice, diceId) => {
+    diceApp.showDice(chosenDice);
+    diceApp.showRoll(chosenDice, diceId);
+}
+
+diceApp.resetButton = () => {
+    $('button').on('click', function(){
+        $('#rollHistory').empty();
+    })
+}
+
 // Initializing the Application
 diceApp.init = function () {
     $('li div').on('click', function() {
         const diceId = $(this).attr('id');
         const chosenDice = diceApp.randomDiceNumber(diceId);
-        diceApp.showDice(chosenDice);
-    });    
+        diceApp.showMainScreen(chosenDice, diceId);
+    });
+
+    diceApp.resetButton();
 }
+
+
 
 // Document Ready Function
 $(function() {
