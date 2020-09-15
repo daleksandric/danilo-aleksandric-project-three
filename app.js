4cc534c6fdc5ff720329ff6af90181c64b3d30ec
Danilo Aleksandric <aleksandric.danilo@gmail.com>
2020-09-14 22:23:23 -0400

    finished project with mobile responsiveness built in

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ diceApp.init = function () {
     diceApp.resetButton();
 }
 
+// This function gets the ID of each clicked button (dice), saves it, then passes it as a parameter to the function which randomizes a number ( from 1 to diceID which indicates the number of sides). This is stored in another variable, that is then passed along with the diceID to a function displaying the end result.
 diceApp.diceClickHandler = () => {
     $('li button').on('click', function() {
         const diceId = $(this).attr('id');
@@ -14,10 +15,13 @@ diceApp.diceClickHandler = () => {
     });
 }
 
+// This is the function that randomizes a number from 1, to diceID. Which is, 1 to 4, or 1 to 6, etc. Based on the dice.
 diceApp.randomDiceNumber = (diceId) => {
     return Math.ceil(Math.random() * diceId);
 }
 
+// This function takes in the ID of each dice (how many sides it has) and the randomized value of the chosen dice (chosenDice). It then adds elements to the HTML which display the result to the user.
+// It also adds some animations in the form of a 'fadeIn' class.
 diceApp.showDice = (chosenDice, diceId) => {
     $('#diceResult').html(`
     <div>
@@ -25,13 +29,14 @@ diceApp.showDice = (chosenDice, diceId) => {
         <p>${chosenDice}</p>
     </div>
     `);
-    
+
      $('#diceResult').addClass('fadeIn');
      setTimeout(() => {
          $('#diceResult').removeClass('fadeIn');
     }, 1500);
 }
 
+// This function displays the chosen dice and the result, recording it in a table so the viewer has access to previously rolled results.
 diceApp.showRoll = (chosenDice, diceId) => {
     const diceRolled = $('<p>').html(`Rolling 1d${diceId}:`);
     const numberResult = $('<p>').html(`${chosenDice}`);
@@ -39,11 +44,13 @@ diceApp.showRoll = (chosenDice, diceId) => {
     $('#rollHistory').append(diceRolled, numberResult);
 }
 
+// This is the main function which calls the ShowDice and ShowRoll functions, which is then passed into the diceClickHandler that triggers it on an "On Click" event.
 diceApp.showMainScreen = (chosenDice, diceId) => {
     diceApp.showDice(chosenDice, diceId);
     diceApp.showRoll(chosenDice, diceId);
 }
 
+// This is a simple function for the button which empties the table of any HTML elements, effectively "resetting" it in the absence of any local storage.
 diceApp.resetButton = () => {
     $('.resetButton').on('click', function(){
         $('#rollHistory').empty();
