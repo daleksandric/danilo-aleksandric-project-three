5cfe6d045219d7145ee3f696b70f700b234877c1
Danilo Aleksandric <aleksandric.danilo@gmail.com>
2020-09-14 21:29:27 -0400

    MVP achieved; pre-mobile responsive

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
 const diceApp = {};
 
-diceApp.randomDiceNumber = (diceSides) => {
-    return Math.ceil(Math.random() * diceSides);
+// Initializing the Application
+diceApp.init = function () {
+    diceApp.diceClickHandler();
+    diceApp.resetButton();
 }
 
-diceApp.showDice = (chosenDice) => {
-    $('#diceResult').html(`<div>${chosenDice}</div>`);
+diceApp.diceClickHandler = () => {
+    $('li button').on('click', function() {
+        const diceId = $(this).attr('id');
+        const chosenDice = diceApp.randomDiceNumber(diceId);
+        diceApp.showMainScreen(chosenDice, diceId);
+    });
+}
+
+diceApp.randomDiceNumber = (diceId) => {
+    return Math.ceil(Math.random() * diceId);
+}
+
+diceApp.showDice = (chosenDice, diceId) => {
+    $('#diceResult').html(`
+    <div>
+        <img src="./assets/${diceId}sided-md.png">
+        <p>${chosenDice}</p>
+    </div>
+    `);
+    
+     $('#diceResult').addClass('fadeIn');
+     setTimeout(() => {
+         $('#diceResult').removeClass('fadeIn');
+    }, 1500);
 }
 
 diceApp.showRoll = (chosenDice, diceId) => {
-    const diceRolled = $('<p>').html(`Rolling 1d${diceId}`);
+    const diceRolled = $('<p>').html(`Rolling 1d${diceId}:`);
     const numberResult = $('<p>').html(`${chosenDice}`);
 
     $('#rollHistory').append(diceRolled, numberResult);
 }
 
 diceApp.showMainScreen = (chosenDice, diceId) => {
-    diceApp.showDice(chosenDice);
+    diceApp.showDice(chosenDice, diceId);
     diceApp.showRoll(chosenDice, diceId);
 }
 
 diceApp.resetButton = () => {
-    $('button').on('click', function(){
+    $('.resetButton').on('click', function(){
         $('#rollHistory').empty();
     })
 }
-
-// Initializing the Application
-diceApp.init = function () {
-    $('li div').on('click', function() {
-        const diceId = $(this).attr('id');
-        const chosenDice = diceApp.randomDiceNumber(diceId);
-        diceApp.showMainScreen(chosenDice, diceId);
-    });
-
-    diceApp.resetButton();
-}
-
 
 
 // Document Ready Function
